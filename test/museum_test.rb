293a42d09de937919538b2075e219cd8ed0e2081
Nick King <nickmaxking@gmail.com>
2020-09-10 09:28:19 -0600

    Fix typo in add exhibit test assert statement

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -20,6 +20,7 @@ class MuseumTest < Minitest::Test
 
   def test_it_can_add_exhibits
     dmns = Museum.new("Denver Museum of Nature and Science")
+
     gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     imax = Exhibit.new({name: "IMAX",cost: 15})
@@ -28,7 +29,6 @@ class MuseumTest < Minitest::Test
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
 
-    assert_equal [gems_and_minerals, dead_sea_scrolls, imax]. dmns.exhibits
-
+    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
   end
 end
