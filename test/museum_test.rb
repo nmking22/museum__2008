1aebf7d019435fc746d2f4bcd80e18cd3863a3e3
Nick King <nickmaxking@gmail.com>
2020-09-10 10:09:51 -0600

    Add test for draw lottery winner

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -146,4 +146,35 @@ class MuseumTest < Minitest::Test
 
     assert_equal [patron_1, patron_3], dmns.ticket_lottery_contestants(dead_sea_scrolls)
   end
+
+  def test_it_can_draw_lottery_winner
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    dnms.ticket_lottery_contestants
+
+    assert_equal (patron_1 || patron_2), dnms.draw_lottery_winner(dead_sea_scrolls)
+    assert_equal nil, dnms.draw_lottery_winner(gems_and_minerals)
+  end
 end
